928f30e39582f2f0399fd86d02823df27e6964eb
nyenz-jona <nyenzjona@gmail.com>
2024-12-24 06:38:59 +0300

    Add files via upload

---
+++ c programming prac course work.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main(){
+
+
+    double quantity;
+    double buyingprice;
+    double profit;
+    double sellingprice;
+
+
+    printf("Enter quantity of item;");
+    scanf("%lf",&quantity);
+
+    printf("what was the buying price per unit?;");
+    scanf("%lf",&buyingprice);
+
+    printf("what was the selling price per unit?:");
+    scanf("%lf",&sellingprice);
+
+    profit = ( sellingprice - buyingprice) *quantity;
+
+
+    if(profit<0){
+        printf("You made a loss of %0.2lf\n",profit);
+    }
+
+
+
+return 0;
+
+
+}
